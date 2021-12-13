2fcdfa02c9da44359b2f5557bf970c71fbd8f0ec
It_salemm <salemolorundare@gmail.com>
2021-12-13 01:54:55 -0800

    Initial commit

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const {checkUser, requireAuth, requireMonoReauthToken} = require('./middleware/a
 
 const app = express();
 
+ mongoose.set('useCreateIndex', true);
 const dbURI = 'mongodb+srv://Salem:Vybranium@test.oepe0.mongodb.net/Vybe?retryWrites=true&w=majority';
 
 mongoose.connect( dbURI,  { useNewUrlParser: true, useUnifiedTopology: true })
